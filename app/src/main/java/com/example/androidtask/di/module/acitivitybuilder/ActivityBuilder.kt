d24a633aace81b0dc06301f64ce1e41efb417cac
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-08 23:13:06 +0200

    Create ActivityBuilder.kt

---
+++ app/src/main/java/com/example/androidtask/di/module/acitivitybuilder/ActivityBuilder.kt
@@ -0,0 +1,16 @@
+package com.example.androidtask.di.module.acitivitybuilder
+
+import dagger.Module
+
+/**
+ * Created by Yasmine on September,2021
+ */
+@Module
+abstract class ActivityBuilderModule {
+
+//    @MainScope
+//    @ContributesAndroidInjector(modules = [MainActivityModule::class,MainViewModelModule::class, MainFragmentBuildersModule::class])
+//    abstract fun contributesInjectMainActivity(): MainActivity
+
+
+}
