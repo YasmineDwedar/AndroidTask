7d82e61f424f092d3e9a897075be2eab63a7936b
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-08 23:12:24 +0200

    adding Base Classes

---
+++ app/src/main/java/com/example/androidtask/application/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.example.androidtask.application
+
+import com.example.androidtask.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+/**
+ * Created by Yasmine on September,2021
+ */
+class BaseApplication : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().application(this).build()
+    }
+}
---
+++ app/src/main/java/com/example/androidtask/base/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.androidtask.base
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Yasmine on September,2021
+ */
+open class BaseViewModel : ViewModel() {
+
+}
