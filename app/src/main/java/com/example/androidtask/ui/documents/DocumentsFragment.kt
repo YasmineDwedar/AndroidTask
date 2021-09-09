7a6b07e713fa5027371114903c26d95c2ee58f7f
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-09 13:44:56 +0200

    DocumentsFragment created

---
+++ app/src/main/java/com/example/androidtask/ui/documents/DocumentsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.androidtask.ui.documents
+
+
+import com.example.androidtask.R
+import com.example.androidtask.base.BaseFragment
+import com.example.androidtask.databinding.FragmentDocumentsBinding
+
+
+class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragmentViewModel>(R.layout.fragment_documents) {
+
+    override fun setListeners() {
+
+    }
+
+    override fun setObservers() {
+    }
+
+    override fun initializeViewModel() {
+    }
+
+}
