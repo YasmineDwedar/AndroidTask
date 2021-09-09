fef8fdd0dc9f4210b17cb0cce707a8ca6fbcb0cf
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-09 13:46:06 +0200

    Create DocumentsScope.kt

---
+++ app/src/main/java/com/example/androidtask/di/scopes/DocumentsScope.kt
@@ -0,0 +1,11 @@
+package com.example.androidtask.di.scopes
+
+import javax.inject.Scope
+
+/**
+ * Created by Yasmine on September,2021
+ */
+@Scope
+@MustBeDocumented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class DocumentsScope
