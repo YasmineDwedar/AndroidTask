76710961a2a93bb20410d24cc6522523623da93c
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-09 21:47:49 +0200

    some useful ExtensionFunctions

---
+++ app/src/main/java/com/example/androidtask/utils/extensions/EditTextExtension.kt
@@ -0,0 +1,20 @@
+package com.example.androidtask.utils.extensions
+
+import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+/**
+ * Created by Yasmine on September,2021
+
+ */
+fun EditText.onTextChangedWithDebounce(): Flow<String> = callbackFlow {
+
+    this@onTextChangedWithDebounce.doAfterTextChanged {
+        offer(it.toString())
+    }
+    awaitClose { this@onTextChangedWithDebounce.addTextChangedListener(null) }
+
+}
