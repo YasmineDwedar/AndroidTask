18f54272c25fffb1277845d9863fd23accd5c8d8
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-09 15:38:31 +0200

    Create ActivityExtension.kt

---
+++ app/src/main/java/com/example/androidtask/utils/extensions/ActivityExtension.kt
@@ -0,0 +1,25 @@
+package com.example.androidtask.utils.extensions
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.WindowManager
+
+/**
+ * Created by Yasmine on September,2021
+ */
+
+fun Activity.makeStatusBarTransparent() {
+    window.apply {
+        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        statusBarColor = Color.TRANSPARENT
+    }
+}
