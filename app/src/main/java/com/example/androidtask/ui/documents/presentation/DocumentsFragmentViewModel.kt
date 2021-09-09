e35789ce5e4074a90033906a547ef7ad4caf3ff8
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-09 15:41:15 +0200

    Create DocumentsFragmentViewModel.kt

---
+++ app/src/main/java/com/example/androidtask/ui/documents/presentation/DocumentsFragmentViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.androidtask.ui.documents.presentation
+
+import com.example.androidtask.base.BaseViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Yasmine on September,2021
+ */
+class DocumentsFragmentViewModel @Inject constructor():BaseViewModel() {
+}
