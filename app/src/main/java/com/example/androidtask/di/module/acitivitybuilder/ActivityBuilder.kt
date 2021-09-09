995cb2448d6f8ef71a5fcbd637753d50d378a0d9
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-09 13:51:51 +0200

    Update ActivityBuilder.kt

--- app/src/main/java/com/example/androidtask/di/module/acitivitybuilder/ActivityBuilder.kt
+++ app/src/main/java/com/example/androidtask/di/module/acitivitybuilder/ActivityBuilder.kt
@@ -1,16 +1,20 @@
 package com.example.androidtask.di.module.acitivitybuilder
 
+import com.example.androidtask.di.module.main.MainActivityModule
+import com.example.androidtask.di.module.main.MainFragmentBuildersModule
+import com.example.androidtask.di.module.main.MainViewModelModule
+import com.example.androidtask.di.scopes.MainScope
+import com.example.androidtask.ui.main.MainActivity
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 /**
  * Created by Yasmine on September,2021
  */
 @Module
 abstract class ActivityBuilderModule {
-
-//    @MainScope
-//    @ContributesAndroidInjector(modules = [MainActivityModule::class,MainViewModelModule::class, MainFragmentBuildersModule::class])
-//    abstract fun contributesInjectMainActivity(): MainActivity
-
+    @MainScope
+    @ContributesAndroidInjector(modules = [MainViewModelModule::class, MainFragmentBuildersModule::class, MainActivityModule::class])
+    abstract fun contributesInjectMainActivity(): MainActivity
 
 }
