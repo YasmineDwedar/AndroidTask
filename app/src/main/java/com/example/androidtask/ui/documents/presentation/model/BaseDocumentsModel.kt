7c256c545aceccc7b93c5c3c768ea9c37943326d
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-10 00:49:17 +0200

    handle shimmer addition

---
+++ app/src/main/java/com/example/androidtask/ui/documents/presentation/model/BaseDocumentsModel.kt
@@ -0,0 +1,6 @@
+package com.example.androidtask.ui.documents.presentation.model
+
+/**
+ * Created by Yasmine on September,2021
+ */
+abstract class BaseDocumentsModel
---
+++ app/src/main/java/com/example/androidtask/ui/documents/presentation/model/DocumentPresentationModel.kt
@@ -0,0 +1,9 @@
+package com.example.androidtask.ui.documents.presentation.model
+
+/**
+ * Created by Yasmine on September,2021
+ */
+data class DocumentPresentationModel (
+        val title: String="",
+        val author: String="",
+        )
