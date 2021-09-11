ab8d0534056cdaeba8bbb5f6e2ea62a8fcd74985
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-11 17:04:16 +0200

    Update DocumentsFragmentViewModel.kt

--- app/src/main/java/com/example/androidtask/modules/documents/presentation/DocumentsFragmentViewModel.kt
+++ app/src/main/java/com/example/androidtask/modules/documents/presentation/DocumentsFragmentViewModel.kt
@@ -10,6 +10,8 @@ import com.example.androidtask.commons.presentation.BaseViewModel
 import com.example.androidtask.commons.presentation.extensions.hide
 import com.example.androidtask.commons.presentation.extensions.show
 import com.example.androidtask.modules.documents.domain.mapper.toPresentationModel
+import com.example.androidtask.modules.documents.domain.usecase.SearchByAuthorUseCase
+import com.example.androidtask.modules.documents.domain.usecase.SearchByTitleUseCase
 import com.example.androidtask.modules.documents.domain.usecase.SearchDocumentsUseCases
 import com.example.androidtask.modules.documents.presentation.model.BaseDocumentsModel
 import com.example.androidtask.modules.documents.presentation.model.DocumentPresentationModel
@@ -24,7 +26,11 @@ import javax.inject.Inject
 /**
  * Created by Yasmine on September,2021
  */
-class DocumentsFragmentViewModel @Inject constructor(private val searchDocumentsUseCases: SearchDocumentsUseCases) :
+class DocumentsFragmentViewModel @Inject constructor(
+    private val searchDocumentsUseCases: SearchDocumentsUseCases,
+    private val searchByAuthorUseCase: SearchByAuthorUseCase,
+    private val searchByTitleUseCase: SearchByTitleUseCase
+) :
     BaseViewModel() {
 
     private var _documentsStateFlow = MutableStateFlow<List<BaseDocumentsModel>>(emptyList())
@@ -53,37 +59,31 @@ class DocumentsFragmentViewModel @Inject constructor(private val searchDocuments
 
     fun getDocumentsSearchResults() {
         launchSafeCoroutine {
-
-            /* _documentsStateFlow.emit(getShimmerList())
-             _documentsStateFlow.emit(
-                 searchDocumentsUseCases.execute(search = search, page = page).map {
-                     it.toPresentationModel()
-                 })*/
             controlCall.cancelPreviousThenRun {
                 if (hasNext) {
                     if (pageNumber == 1) _documentsStateFlow.emit(getShimmerList())
-                        val result = searchDocumentsUseCases.execute(searchString, pageNumber)
-                            .map { it.toPresentationModel() }
-                        if (responsee.isEmpty()) {  //First network call
-                            responsee.addAll(result)
+                    val result = searchDocumentsUseCases.execute(searchString, pageNumber)
+                        .map { it.toPresentationModel() }
+                    if (responsee.isEmpty()) {  //First network call
+                        responsee.addAll(result)
+                        pageNumber++
+                        _documentsStateFlow.emit(result)
+                    } else {                  // SecondOrMore network call
+                        if (result.size == ITEMS_PER_PAGE) {
                             pageNumber++
-                            _documentsStateFlow.emit(result)
-                        } else {                  // SecondOrMore network call
-                            if (result.size == ITEMS_PER_PAGE) {
-                                pageNumber++
-                                val oldProducts = responsee
-                                oldProducts.addAll(result)
-                                _documentsStateFlow.emit(oldProducts.toList())
-                            } else if (result.size < ITEMS_PER_PAGE) {
-                                val oldProducts = responsee
-                                oldProducts.addAll(result)
-                                _documentsStateFlow.emit(oldProducts)
-                                hasNext = false
-                                _hasNextStateFlow.emit(false)
-                            }
+                            val oldProducts = responsee
+                            oldProducts.addAll(result)
+                            _documentsStateFlow.emit(oldProducts.toList())
+                        } else if (result.size < ITEMS_PER_PAGE) {
+                            val oldProducts = responsee
+                            oldProducts.addAll(result)
+                            _documentsStateFlow.emit(oldProducts)
+                            hasNext = false
+                            _hasNextStateFlow.emit(false)
                         }
                     }
                 }
+            }
         }
     }
 
@@ -117,6 +117,19 @@ class DocumentsFragmentViewModel @Inject constructor(private val searchDocuments
         }
     }
 
+     fun getSearchResultsFromTitle(title: String) {
+        launchSafeCoroutine {
+           val results= searchByTitleUseCase.execute(title).map { it.toPresentationModel() }
+            _documentsStateFlow.emit(results)
+        }
+    }
+
+     fun getSearchResultsFromAuthor(author: String) {
+        launchSafeCoroutine {
+          val results= searchByAuthorUseCase.execute(author).map { it.toPresentationModel() }
+            _documentsStateFlow.emit(results)
+        }
+    }
 }
 
 
