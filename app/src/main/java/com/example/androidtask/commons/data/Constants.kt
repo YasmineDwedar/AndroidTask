f312725a269ea598b78dff40c5f0241214ddccd2
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-11 16:57:48 +0200

    Update Constants.kt

--- app/src/main/java/com/example/androidtask/commons/data/Constants.kt
+++ app/src/main/java/com/example/androidtask/commons/data/Constants.kt
@@ -6,7 +6,8 @@ package com.example.androidtask.commons.data
 object Constants {
     const val BASE_URL = "http://openlibrary.org/"
     const val SEARCH = "search.json"
+    const val SELECTED_TEXT = "selected_text"
+    const val SELECTED_TEXT_MODE = "selected_text_mode"
     const val ITEMS_PER_PAGE=100
     const val ITEM_DETAIL_BUNDLE_KEY="itemdetail"
-
 }
