6a374ef004a1427f6c1944b40cb120451aebf679
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-11 17:13:16 +0200

    bind DetailsMainCallBack

--- app/src/main/java/com/example/androidtask/modules/details/di/DetailsFragmentModule.kt
+++ app/src/main/java/com/example/androidtask/modules/details/di/DetailsFragmentModule.kt
@@ -1,8 +1,16 @@
 package com.example.androidtask.modules.details.di
 
+import com.example.androidtask.commons.di.scopes.DocumentsScope
+import com.example.androidtask.modules.details.presentation.DetailsFragment
+import com.example.androidtask.modules.details.presentation.recyclerview.MainDetailsAdapter
+import com.example.androidtask.modules.documents.presentation.DocumentsFragment
+import com.example.androidtask.modules.documents.presentation.recyclerview.DocumentsAdapter
+import dagger.Binds
 import dagger.Module
 
 @Module
-class DetailsFragmentModule {
-
+abstract class DetailsFragmentModule {
+    @Binds
+    @DetailsScope
+    abstract fun bindDetailsMainCallBack(detailsFragment: DetailsFragment): MainDetailsAdapter.DetailsMainCallBack
 }
