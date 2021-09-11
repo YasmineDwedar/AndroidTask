21f92159bc78599a4d463775ce2663edb8da7b32
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-11 17:12:59 +0200

    setOnCheckedChangeListener

--- app/src/main/java/com/example/androidtask/modules/documents/presentation/DocumentsFragment.kt
+++ app/src/main/java/com/example/androidtask/modules/documents/presentation/DocumentsFragment.kt
@@ -9,32 +9,34 @@ import android.widget.TextView.OnEditorActionListener
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidtask.R
 import com.example.androidtask.commons.data.Constants.ITEM_DETAIL_BUNDLE_KEY
+import com.example.androidtask.commons.data.Constants.SELECTED_TEXT
+import com.example.androidtask.commons.data.Constants.SELECTED_TEXT_MODE
 import com.example.androidtask.commons.presentation.BaseFragment
 import com.example.androidtask.databinding.FragmentDocumentsBinding
 import com.example.androidtask.modules.documents.presentation.model.BaseDocumentsModel
 import com.example.androidtask.modules.documents.presentation.recyclerview.DocumentsAdapter
 import com.example.androidtask.commons.presentation.extensions.*
+import com.example.androidtask.modules.details.presentation.model.SelectedTextMode
 import com.example.androidtask.modules.documents.presentation.model.DocumentPresentationModel
 import com.github.ybq.android.spinkit.sprite.Sprite
 import com.github.ybq.android.spinkit.style.Circle
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
-class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragmentViewModel>(R.layout.fragment_documents),
+class DocumentsFragment :
+    BaseFragment<FragmentDocumentsBinding, DocumentsFragmentViewModel>(R.layout.fragment_documents),
     DocumentsAdapter.DocumentsMainCallBack {
 
     @Inject
     lateinit var documentsAdapter: DocumentsAdapter
     var isLoading = false
     var isScrolling = false
+    var selectedText: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +49,7 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
         setUpRecyclerView()
         hideCircularProgressBar()
     }
+
     override fun setListeners() {
         binding?.apply {
             searchEt.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
@@ -56,12 +59,21 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
                 }
                 false
             })
-           /* lifecycleScope.launchWhenResumed {
-                searchEt.onTextChangedWithDebounce().debounce(600L).collect {
-                    performSearch(it)
-                }
-            }*/
+
+            titleRadioButton.setOnCheckedChangeListener { _, _ ->
+                viewModel.getSearchResultsFromTitle(searchEt.text.toString())
+
+            }
+            authorRadioButton.setOnCheckedChangeListener { _, _ ->
+                viewModel.getSearchResultsFromAuthor(searchEt.text.toString())
+
+            }
+            allRadioButton.setOnCheckedChangeListener { _, _ ->
+                viewModel.getDocumentsSearchResults()
+
+            }
         }
+
     }
 
     override fun setObservers() {
@@ -89,7 +101,34 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
                 }
             }
         }
-
+        this.getNavigationResultLiveData<String>(SELECTED_TEXT)
+            ?.observe(viewLifecycleOwner) { result ->
+                Log.d("TAG", "SELECTED: $result")
+                binding?.apply {
+                    selectedText = result
+                    searchEt.setText(result)
+                }
+            }
+        this.getNavigationResultLiveData<SelectedTextMode>(SELECTED_TEXT_MODE)
+            ?.observe(viewLifecycleOwner) { result ->
+                Log.d("TAG", "SELECTED: $result")
+                binding?.apply {
+                    when (result) {
+                        SelectedTextMode.TITLE -> {
+                            titleRadioButton.isChecked = true
+                            selectedText?.let { title ->
+                                viewModel.getSearchResultsFromTitle(title)
+                            }
+                        }
+                        SelectedTextMode.AUTHOR -> {
+                            authorRadioButton.isChecked = true
+                            selectedText?.let { author ->
+                                viewModel.getSearchResultsFromAuthor(author)
+                            }
+                        }
+                    }
+                }
+            }
     }
 
     override fun initializeViewModel() {
@@ -99,6 +138,7 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
             it.lifecycleOwner = viewLifecycleOwner
         }
     }
+
     private fun setUpRecyclerView() {
         binding?.documentsRV?.adapter = documentsAdapter
         binding?.documentsRV?.addOnScrollListener(scrollListner)
@@ -108,20 +148,23 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
         documentsAdapter.submitList(list)
         documentsAdapter.notifyDataSetChanged()
     }
+
     private fun showCircularProgressBar() {
         val doubleBounce: Sprite = Circle()
         binding?.progressBar?.setIndeterminateDrawable(doubleBounce)
         binding?.progressBar?.visibility = View.VISIBLE
-        isLoading=true
+        isLoading = true
     }
 
     private fun hideCircularProgressBar() {
         binding?.progressBar?.visibility = View.GONE
         isLoading = false
     }
+
     fun onScrollPage() {
         viewModel.getDocumentsSearchResults()
     }
+
     val scrollListner = object : RecyclerView.OnScrollListener() {
 
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -129,26 +172,22 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
             if (dy > 0) {
                 try {
                     val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-                    val visibleItemCount = layoutManager.findLastCompletelyVisibleItemPosition()+1
-                    if (visibleItemCount == layoutManager.itemCount && viewModel.hasNext){
+                    val visibleItemCount = layoutManager.findLastCompletelyVisibleItemPosition() + 1
+                    if (visibleItemCount == layoutManager.itemCount && viewModel.hasNext) {
                         onScrollPage()
                         showCircularProgressBar()
-                        Log.d("TAG", "onScrolled: ${visibleItemCount} lastVisibleItemPosition:${layoutManager.itemCount}")
+                        Log.d(
+                            "TAG",
+                            "onScrolled: ${visibleItemCount} lastVisibleItemPosition:${layoutManager.itemCount}"
+                        )
                         isScrolling = false
                     }
 
-                } catch (ex: Exception) {}
+                } catch (ex: Exception) {
+                }
             }
         }
     }
-
-
-
-
-
-
-
-
 
 
     override fun onItemClicked(item: DocumentPresentationModel) {
@@ -162,6 +201,4 @@ class DocumentsFragment : BaseFragment<FragmentDocumentsBinding, DocumentsFragme
             NavOptions.Builder().setLaunchSingleTop(true).build()
         )
     }
-
-
 }
