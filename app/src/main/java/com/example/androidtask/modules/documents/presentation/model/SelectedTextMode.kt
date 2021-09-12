846d91382a0592258ba9106286f77f7facd013f0
YasmineDwedar <yasminedwedar30@gmail.com>
2021-09-12 04:01:11 +0200

    Create SelectedTextMode.kt

---
+++ app/src/main/java/com/example/androidtask/modules/documents/presentation/model/SelectedTextMode.kt
@@ -0,0 +1,10 @@
+package com.example.androidtask.modules.documents.presentation.model
+
+/**
+ * Created by Yasmine on September,2021
+ */
+enum class SelectedTextMode {
+    TITLE,
+    AUTHOR,
+    ALL,
+}
